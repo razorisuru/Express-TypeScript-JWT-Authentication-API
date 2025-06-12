d041ccb8b636189ead42f1dc9fa4b40a699cdbd8
Isuru Bandara <isurubandara318@gmail.com>
2025-06-13 00:11:48 +0530

    Update auth.routes.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -7,6 +7,7 @@ const router = Router();
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
+// Test Route 
 // Example protected route
 // router.get('/protected', authenticate, (req, res) => {
 //   res.json({ message: `Hello ${req.user.username}, you are authenticated.` });
